2fb264a156313e84666c6b641687481208003dba
Varun <varunk3249@gmail.com>
2020-06-01 18:19:14 -0400

    added some functions

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,26 @@
+#Main Code here
 
+#importing libraries
+import numpy as np
+import random
+
+
+#functions
+
+#Creates a board
+def create_board():
+    return(np.array([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]]))
+
+# Check for empty places on board
+#Copied this from interent... you can change if you want
+def possibilities(board):
+    l = []
+      
+    for i in range(len(board)):
+        for j in range(len(board)):
+              
+            if board[i][j] == 0:
+                l.append((i, j))
+    return(l)
