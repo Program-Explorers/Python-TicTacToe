1921d926ac146b2aa1650e18181226064a28b58e
Varun <varunk3249@gmail.com>
2020-06-02 22:19:00 -0400

    Cleaned up the code by adding functions and calling them instead of having them directly in Main. Also made sure user's input was valid. It will keep asking for input until it gets a valid one.
    - varun

--- tictactoe.py
+++ tictactoe.py
@@ -21,6 +21,36 @@ def print_board(board):
 
 #Make function here: Chooses a random place for the computer
 
+#Ask for user_input and validate it
+def user_choice_row(char):
+    choice_row = 'WRONG'
+    
+    while choice_row.isdigit() == False:
+    
+        choice_row = input(f"\nType in the ROW you would like to place your '{char}': ")
+        
+        if choice_row.isdigit() == False:
+            print('Sorry that is not a valid number. Please type a number from 1 and 3')
+            
+    return int(choice_row)
+    
+
+def user_choice_col(char):
+    choice_col = 'NOPE'
+    
+    while choice_col.isdigit() == False:
+        
+        choice_col = input(f"Type in the COLUMN you would like to place your '{char}': ")
+        
+        if choice_col.isdigit() == False:
+            print('Sorry that is not a valid number. Please type a number from 1 and 3')
+            
+    return int(choice_col)
+    
+    
+
+    
+    
 #checks if the user won by a row
 
 def row_win():
@@ -33,7 +63,20 @@ def diag_win():
     pass
     
 def random_char():
-    return random.randint(1,2)
+    result = random.randint(1,3)
+    
+    if result == 1:
+        print('\nYou are X!\n')
+        character = 'X'
+        
+    elif result == 2:
+        print('\nYou are O!\n')
+        character = 'O'
+        
+    else:
+        print('Error in choosing your character XO')
+        
+    return character
    
 def main():
 
@@ -41,22 +84,11 @@ def main():
     user_wins = 0
     comp_wins = 0
     
-    #assigns randome number 1 or 2 to result
-    result = random_char()
     #Greeting
     print('\n\nWelcome to Tic Tac Toe!\nYou will be playing with a computer!\nFirst to three wins')
 
-    #Randomizes character assignment X or O
-    if result == 1:
-        print('\nYou are X!\n')
-        character = 'X'
-        
-    elif result == 2:
-        print('\nYou are Y!\n')
-        character = 'X'
-        
-    else:
-        print('Error in choosing your character XO')
+    #Calls function and gets the random character X or O, and assigns it to the variable
+    character = random_char()
     
     for turn in range(10):
         print_board(board)
@@ -64,9 +96,10 @@ def main():
         print('\n Turn:', turn+1)
 
         #Gathers input for row and column
-        input_row = int(input(f"\nType in the ROW you would like to place your '{character}': "))
-        input_col = int(input(f"Type in the COLUMN you would like to place your '{character}':"))
-
+    
+        input_row = user_choice_row(character)
+        
+        input_col = user_choice_col(character)
     
         if user_wins == 3:
             print('You have won! Great Job!')
